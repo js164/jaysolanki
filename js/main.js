4502a832d91a598d6adade727d30d1eb3634eafa
Jay Solanki <j194solanki@gmail.com>
2025-05-22 16:05:24 +0200

    updated grade

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing", {
-    strings: ["Software Developer","Computer Engineer", "AWS Solution Architect"],
+    strings: ["Software Developer", "AWS Solution Architect"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
